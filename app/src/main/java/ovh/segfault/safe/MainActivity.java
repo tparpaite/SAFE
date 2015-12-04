746a5b6eade809d9f487ddc728568dc0ff43b1fe
piecougouilles <pierre.cougouilles@etu.u-bordeaux1.fr>
2015-12-04 06:47:27 +0100

    Ensemble des travaux de Charles

--- app/src/main/java/ovh/segfault/safe/MainActivity.java
+++ app/src/main/java/ovh/segfault/safe/MainActivity.java
@@ -1,5 +1,6 @@
 package ovh.segfault.safe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView ;//added now
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,66 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Tremblement de terre",
+                                         "Inondation ou Tsunami",
+                                         "Catastrophe industrielle",
+                                         "Ouragan",
+                                         "Incendie",
+                                         "Attentat",
+                                         "Gestes de premiers secours",
+                                      };
+        final Class[] names = new Class[] {
+                EarthquakeActivity.class,
+                TsunamiActivity.class,
+                IndusActivity.class,
+                WindActivity.class,
+                FireActivity.class,
+                AssaultActivity.class,
+                FirstaidActivity.class,
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+              android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                Intent intent = new Intent(MainActivity.this, names[position]);
+                //Log.d("test", EarthquakeActivity.class.getName());
+                //EditText editText = (EditText) findViewById(R.id.edit_message);
+                //String message = editText.getText().toString();
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+
+                // ListView Clicked item index
+                ////////////int itemPosition     = position;
+
+                // ListView Clicked item value
+                ////////////////String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                //////////////Toast.makeText(getApplicationContext(), "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -41,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -49,4 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
